1c4cd845227f148d7e4743cf734fa126cfe408ef
Jnroop0318 <63819272+Jnroop0318@users.noreply.github.com>
2022-08-28 22:23:08 -0500

    Commit User

---
+++ Controlador-financiero/src/main/java/Model/User.java
@@ -0,0 +1,58 @@
+package Model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+public class User <Enterprise> {
+    private long id;
+
+    private String email;
+
+    private profile profile;
+
+    private Enum_RoleName role;
+
+    private Enterprise enterprise;
+
+    private List<Transaction> transaction;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    public User(long id, String email, profile profile, Enum_RoleName role, Enterprise enterprise, List<Transaction> transaction, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.email = email;
+        this.profile = profile;
+        this.role = role;
+        this.enterprise = enterprise;
+        this.transaction = transaction;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(profile profile) {
+        this.profile = profile;
+    }
+}
