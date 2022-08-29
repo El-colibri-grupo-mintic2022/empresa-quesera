4bdb91d812ee2521729f7fbb5d0a209873553060
Javic97 <111356022+Javic97@users.noreply.github.com>
2022-08-28 20:42:19 -0500

    Commit Transaction2

---
+++ Controlador-financiero/src/main/java/Model/Transaction.java
@@ -0,0 +1,93 @@
+package Model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class Transaction {
+    private long id;
+
+    private String concept;
+
+    private float amount;
+
+    private user user;
+
+    private E enterprise;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime  updatedAt;
+
+    //Constructor
+
+    public Transaction(long id, String concept, float amount, user user, Enterprise enterprise, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        super();
+        this.id = id;
+        this.concept = concept;
+        this.amount = amount;
+        this.user = user;
+        this.Enterprise = enterprise;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    //Encapsulamiento
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getConcept() {
+        return concept;
+    }
+
+    public void setConcept(String concept) {
+        this.concept = concept;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public user getUser() {
+        return user;
+    }
+
+    public void setUser(user user) {
+        this.user = user;
+    }
+
+    public Enterprise getEnterprise() {
+        return Enterprise;
+    }
+
+    public void setEnterprise(Enterprise enterprise) {
+        this.Enterprise = enterprise;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
