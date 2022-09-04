171e8c9dddf852bd3a6c9808d2683507493ec6dc
Albertosb10 <albertosb10@outlook.com>
2022-09-04 11:38:39 -0500

    creacion controlador y repositorio de la clase enterprise

--- controlador-finaniero 2/src/main/java/elcolibri/controladorfinaniero/controller/EnterpriseController.java
+++ controlador-finaniero 2/src/main/java/elcolibri/controladorfinaniero/controller/EnterpriseController.java
@@ -1,4 +1,13 @@
 package elcolibri.controladorfinaniero.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/enterprise")
 public class EnterpriseController {
+
+
+
 }
